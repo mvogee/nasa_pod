003693305c6cca1cab3e96c8cf43c9c0b7dc4de8
Matthew Vogee <mvogee@yahoo.com>
2021-04-05 16:53:39 -0700

    html, css, and images moved to public folder and served statically

---
+++ public/scripts.js
@@ -0,0 +1,8 @@
+alert("this is working");
+var header = document.querySelector(".title");
+var explanation = document.querySelector(".explanation");
+var copyright = document.querySelector(".copyright");
+
+header.innerHTML = "Yo this is workin!";
+explanation.innerHTML = "Yo this is also working!";
+copyright.innerHTML = "yo this is the copyright!";
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+//TODO: figure out how to get the image to display on the html page after it is downloaded
+//TODO: figure out how to get the title and explanation from the server to the page
+//TODO: figure out when you should download the image. can we avoid having to download it on every request to the server? like donwload it once a day and store it in public where the client can access it as soon as the content is sent?
+
+
 const express = require("express");
 const app = express();
 const keys = require("./keys.js");// local file containing api key object
 const https = require("https");
 const fs = require("fs");
 const Stream = require("stream").Transform;
-const { response } = require("express");
+//const { response } = require("express");
 
 const port = 3000;
-
-function donwloadImg(url) {
+app.use(express.static("./public")); // this is the static available folder
+//! send the public folder with the basic html and css and javascript
+//! the javascirpt will make the get request to here for the nasa content
+//! at that point you can set the page contnet based on the content grabbed from the server
+//!
+// takes the url of the image and downloads the image to the public folder on the server.
+function donwloadImg(url) { // could make this function only run once a day and update the image??
     https.request(url, (res) => {
         var data = new Stream();
         res.on("data", (chunk) => {
@@ -26,23 +36,23 @@ function donwloadImg(url) {
 }
 
 app.get("/", (req, res) => {
-    res.send("this is the response");
     var data = "";
+    console.log("A get request is made!");
     const url = "https://api.nasa.gov/planetary/apod?api_key=" + keys.apiKeys.nasa;
     console.log(url);
-    https.get(url, (res) => {
-        res.on("data", (chunk) => {
+    https.get(url, (response) => {
+        response.on("data", (chunk) => {
             data += chunk;
         });
-        res.on("end", () => {
+        response.on("end", () => {
             const jsonData = JSON.parse(data);
             console.log(jsonData.hdurl);
             donwloadImg(jsonData.hdurl);
-        })
+        });
     }).on("error", (error) => {
         console.log(error);
     });
-    
+    res.sendFile(__dirname + "/public/index.html");
    // console.log(keys.apiKeys.nasa); // this gets the api key from my keys.js file
     // get data from nasa api
 });
